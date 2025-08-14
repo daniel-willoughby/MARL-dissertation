653e8ff881e640d57f21822ea4b3c2947e8c7499
Dan <d.t.willoughby@hotmail.co.uk>
2025-08-14 19:19:53 +0100

    trial 1

---
+++ main.py
@@ -0,0 +1,27 @@
+from pettingzoo.classic import tictactoe_v3
+
+env = tictactoe_v3.env()
+env.reset()
+count = 0
+
+for agent in env.agent_iter():
+    observation, reward, termination, truncation, info = env.last()
+    if termination or truncation:
+        print("looped1")
+        action = 1
+    else:
+        print("looped2")
+        action = env.action_space(agent).sample()  # This is where you would insert your agent's policy
+
+    env.step(action)
+
+    env = tictactoe_v3.env(render_mode="human")
+    env.reset()
+    for _ in range(100):
+        env.render()
+        #env.step(env.action_space(env.agent_selection).sample())
+        env.step(count)
+        count += 1
+        print("looped3")
+    env.close()
+    print("loop complete")
